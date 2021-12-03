5032b4c2493588d29d38c363c4819cb067fcd0dc
Codevs <gdavid.ptorrez@gmail.com>
2021-12-02 20:23:44 -0400

    FOX: correct solution with all casesP

--- adventJS/Exercise2.js
+++ adventJS/Exercise2.js
@@ -1,7 +1,7 @@
 // 02. Help to Elf to count gifts
 const carta = 'bici coche balón  _playstation bici coche peluche';
 function listGifts(letter) {
-  const data = letter.split(/[\s,]+/gi).reduce((prev, curr) => {
+  const data = letter.trim().split(/[\s,]+/gi).reduce((prev, curr) => {
     if(!curr.startsWith('_')) {
          prev[curr] = (prev[curr] || 0) + 1
     }
